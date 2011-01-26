6198705a34603d7124393cde80cd11f23d440368
Brandon Kelly <me@brandon-kelly.com>
2011-01-25 17:37:25 -0800

    major cleanup, and stop dealing with comment subscriptions

--- ext.matrix_comments.php
+++ ext.matrix_comments.php
@@ -17,11 +17,6 @@ class Matrix_comments_ext {
 	var $settings_exist = 'n';
 	var $docs_url       = 'http://github.com/brandonkelly/matrix_comments';
 
-	var $db_column_name                     = 'matrix_row_id';
-	var $template_key_name                  = 'matrix_row_id';
-	var $comments_table_name                = 'comments';
-	var $comment_subscriptions_table_name   = 'comment_subscriptions';
-
 	/**
 	 * Class Constructor
 	 */
@@ -34,153 +29,48 @@ class Matrix_comments_ext {
 	// --------------------------------------------------------------------
 
 	/**
-	 * Column Exists?
-	 */
-	private function _column_exists($column, $table)
-	{
-		return $this->EE->db->field_exists($column, $table);
-	}
-
-	/**
-	 *
-	 */
-	private function _get_rowid()
-	{
-		$row_id = NULL;
-		if(isset($this->EE->TMPL))
-		{
-			$row_id = $this->EE->TMPL->fetch_param($this->template_key_name);
-		}
-
-		if($row_id !== TRUE)
-		{
-			$row_id = $this->EE->input->post($this->template_key_name);
-
-			if(! $row_id)
-			{
-				$row_id = $this->_get_rowid_from_querystring();
-			}
-		}
-
-		return $row_id;
-	}
-
-	/**
-	 * Get row_id from querystring
-	 */
-	private function _get_rowid_from_querystring()
-	{
-		$qstring = $this->EE->uri->query_string;
-
-		if (preg_match("#(^|/)P(\d+)(/|$)#", $qstring, $match))
-		{
-			$qstring = trim($this->EE->functions->remove_double_slashes(str_replace($match['0'], '/', $qstring)), '/');
-		}
-
-		// Figure out the right row ID
-		$row_seg = trim($qstring);
-
-		$row_seg = preg_replace("#.+/#", "", $row_seg);
-
-		if (is_numeric($row_seg))
-		{
-			$row_id = $row_seg;
-		}
-		else
-		{
-			$row_id = '';
-		}
-
-		return $row_id;
-	}
-
-	private function _add_ext_hook_to_db($method_name, $hook_name)
-	{
-		$this->EE->db->insert('extensions', array(
-			'class'    => 'Matrix_comments_ext',
-			'method'   => $method_name,
-			'hook'     => $hook_name,
-			'settings' => '',
-			'priority' => 10,
-			'version'  => $this->version,
-			'enabled'  => 'y'
-		));
-	}
-
-	private function _add_db_column($column_name, $table_name)
-	{
-		// no need to add our custom column if it already exists.
-		if (! $this->_column_exists($column_name, $table_name))
-		{
-			$this->EE->load->dbforge();
-
-			$this->EE->dbforge->add_column(
-				$table_name,
-				array(
-					$column_name => array(
-						'type' => 'INT',
-						'constraint' => 10,
-						'unsigned' => TRUE,
-						'null' => TRUE)
-				)
-			);
-		}
-	}
-
-	private function _drop_db_column($column_name, $table_name)
-	{
-		if ($this->_column_exists($column_name, $table_name))
-		{
-			$this->EE->load->dbforge();
-			$this->EE->dbforge->drop_column($table_name, $column_name);
-		}
-
-	}
-
-	// --------------------------------------------------------------------
-
-	/**
 	 * Activate Extension
 	 */
 	function activate_extension()
 	{
-		$this->_add_db_column($this->db_column_name, $this->comments_table_name);
-		$this->_add_db_column($this->db_column_name, $this->comment_subscriptions_table_name);
+		// -------------------------------------------
+		//  Add the matrix_row_id column to exp_comments
+		// -------------------------------------------
 
-		$ext_hook_data = array(
-								array(
-									'method' => 'insert_comment_insert_array',
-									'hook' => 'insert_comment_insert_array'
-								),
-								array(
-									'method' => 'comment_entries_query',
-									'hook' => 'comment_entries_query'
-								),
-								array(
-									'method' => 'comment_subscription_delete',
-									'hook' => 'comment_subscription_delete'
-								),
-								array(
-									'method' => 'comment_subscription_insert',
-									'hook' => 'comment_subscription_insert'
-								),
-								array(
-									'method' => 'comment_subscription_issubscribed_query',
-									'hook' => 'comment_subscription_issubscribed_query'
-								),
-								array(
-									'method' =>'comment_subscription_query',
-									'hook' => 'comment_subscription_query'
-								),
-								array(
-									'method' => 'comment_subscription_notificationsent_update',
-									'hook' => 'comment_subscription_notificationsent_update'
-								)
-							);
-		// add necessary rows to exp_extensions
-		foreach($ext_hook_data as $row)
+		if (! $this->EE->db->field_exists('matrix_row_id', 'comments'))
 		{
-			$this->_add_ext_hook_to_db($row['method'], $row['hook']);
+			$this->EE->load->dbforge();
+
+			$this->EE->dbforge->add_column('comments', array(
+				'matrix_row_id' => array(
+					'type' => 'INT',
+					'constraint' => 10,
+					'unsigned' => TRUE,
+					'null' => TRUE
+				)
+			));
+		}
+
+		// -------------------------------------------
+		//  Add the extension hooks
+		// -------------------------------------------
+
+		$hooks = array(
+			'comment_entries_query',
+			'insert_comment_insert_array'
+		);
+
+		foreach($hooks as $hook)
+		{
+			$this->EE->db->insert('extensions', array(
+				'class'    => get_class($this),
+				'method'   => $hook,
+				'hook'     => $hook,
+				'settings' => '',
+				'priority' => 10,
+				'version'  => $this->version,
+				'enabled'  => 'y'
+			));
 		}
 	}
 
@@ -198,66 +88,35 @@ class Matrix_comments_ext {
 	 */
 	function disable_extension()
 	{
-		$this->_drop_db_column($this->db_column_name, $this->comments_table_name);
-		$this->_drop_db_column($this->db_column_name, $this->comment_subscriptions_table_name);
+		// -------------------------------------------
+		//  Drop the matrix_row_id column from exp_comments
+		// -------------------------------------------
 
-		// Remove all Matrix_comments_ext rows from exp_extensions
-		$this->EE->db->where('class', 'Matrix_comments_ext')
+		if ($this->EE->db->field_exists('matrix_row_id', 'comments'))
+		{
+			$this->EE->load->dbforge();
+			$this->EE->dbforge->drop_column('comments', 'matrix_row_id');
+		}
+
+		// -------------------------------------------
+		//  Delete the extension hooks
+		// -------------------------------------------
+
+		$this->EE->db->where('class', get_class($this))
 		             ->delete('exp_extensions');
 	}
 
 	// --------------------------------------------------------------------
 
-	/*
-	 * Multiple hooks use this same logic
-	 */
-	function apply_rowid_logic_to_query()
-	{
-		$row_id = $this->_get_rowid();
-
-		// was it actually set to anything?
-		if ($row_id)
-		{
-			// only show comments for that row
-			$this->EE->db->where($this->db_column_name, $row_id);
-		}
-		else
-		{
-			// only grab comments that aren't for a Matrix row
-			$this->EE->db->where("`{$this->db_column_name}` IS NULL", NULL, FALSE);
-		}
-	}
-
 	/**
-	* comment_entries_query ext hook
-	*/
+	 * comment_entries_query ext hook
+	 */
 	function comment_entries_query()
 	{
-		$this->apply_rowid_logic_to_query();
-	}
+		// get the row ID right from the {exp:comment:entries}' matrix_row_id= param
+		$row_id = $this->EE->TMPL->fetch_param('matrix_row_id');
 
-	/**
-	* comment_subscription_issubscribed_query ext hook
-	*/
-	function comment_subscription_issubscribed_query()
-	{
-		$this->apply_rowid_logic_to_query();
-	}
-
-	/**
-	* comment_subscription_query ext hook
-	*/
-	function comment_subscription_query()
-	{
-		$this->apply_rowid_logic_to_query();
-	}
-
-	/**
-	* comment_subscription_notificationsent_update ext hook
-	*/
-	function comment_subscription_notificationsent_update()
-	{
-		$this->apply_rowid_logic_to_query();
+		$this->_apply_row_id_to_query($row_id);
 	}
 
 	/**
@@ -272,46 +131,11 @@ class Matrix_comments_ext {
 			$data = $this->EE->extensions->last_call;
 		}
 
-		$row_id = $this->_get_rowid();
+		$row_id = $this->EE->input->post('matrix_row_id');
 
 		if ($row_id)
 		{
-			$data[$this->db_column_name] = $row_id;
-		}
-
-		return $data;
-	}
-
-	/**
-	 * comment_subscription_delete ext hook
-	 */
-	function comment_subscription_delete()
-	{
-		$this->apply_rowid_logic_to_query();
-	}
-
-	/**
-	 * comment_subscription_insert ext hook
-	 */
-	function comment_subscription_insert($data)
-	{
-		// If another extension shares the same hook,
-		// we need to get the latest and greatest config
-		if ($this->EE->extensions->last_call !== FALSE)
-		{
-			$data = $this->EE->extensions->last_call;
-		}
-
-		$row_id = $this->_get_rowid();
-
-		// did they pass a matrix_row_id= param?
-		if ($row_id !== FALSE)
-		{
-			// was it actually set to anything?
-			if ($row_id)
-			{
-				$data[0][$this->db_column_name] = $row_id;
-			}
+			$data['matrix_row_id'] = $row_id;
 		}
 
 		return $data;
